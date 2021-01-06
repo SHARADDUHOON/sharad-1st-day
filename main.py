d198a0e043ca97148fb1f31b7c165601eefa83bf
SHARAD DUHOON <sharadduhoon97@gmail.com>
2021-01-06 14:41:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("hello world\nhello world \nhello world")
+print("Day 1 - String Manipultion")
+print("Concactenation is done with "+"sign,")
+print('eg = print("hello" + "world")')
+print("New line will be created by using a backslash and n")
+print("hello"+" "+ input("wht is your name?\n"))
+print(len(input(" what is your name?")))
+a = (int(input("enter a no")))
+b = (int(input("enter a no")))
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
+print("Welcome to the Band Name Generator")
+x=input("Enter the city in which you are born? \n")
+y=input("Enter the name of your favourite pet?\n")
+print(" the name of the band is",x+" "+ y)
